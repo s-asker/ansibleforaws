54e71881f679ab6e0c8e24532ace297586ae19ed
s-asker <sherifasker2@gmail.com>
2024-10-13 23:29:57 +0300

    Fixing peering

--- get_instance_id.sh
+++ get_instance_id.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Get the token
+# Get the token for metadata access
 TOKEN=$(curl -X PUT -H "X-aws-ec2-metadata-token-ttl-seconds: 300" http://169.254.169.254/latest/api/token)
 
-# Get the instance ID using the token
-INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-giid -H "X-aws-ec2-metadata-token: $TOKEN")
+# Get the instance ID
+INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id -H "X-aws-ec2-metadata-token: $TOKEN")
 
-# Get the region using the token
+# Get the region
 REGION=$(curl http://169.254.169.254/latest/meta-data/placement/availability-zone -H "X-aws-ec2-metadata-token: $TOKEN" | sed 's/[a-z]$//')
 
-# Fetch instance details using AWS CLI
+# Fetch the VPC ID using AWS CLI
 VPC_ID=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --region $REGION --query "Reservations[0].Instances[0].VpcId" --output text)
 
 # Output the VPC ID
