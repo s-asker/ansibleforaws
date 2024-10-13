ae3d87f85cd58f7cd5c8b9b184fef1485d3d7377
s-asker <sherifasker2@gmail.com>
2024-10-13 23:17:42 +0300

    Fixing peering

--- get_instance_id.sh
+++ get_instance_id.sh
@@ -4,7 +4,7 @@
 TOKEN=$(curl -X PUT -H "X-aws-ec2-metadata-token-ttl-seconds: 300" http://169.254.169.254/latest/api/token)
 
 # Get the instance ID using the token
-INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id -H "X-aws-ec2-metadata-token: $TOKEN")
+INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-giid -H "X-aws-ec2-metadata-token: $TOKEN")
 
 # Get the region using the token
 REGION=$(curl http://169.254.169.254/latest/meta-data/placement/availability-zone -H "X-aws-ec2-metadata-token: $TOKEN" | sed 's/[a-z]$//')
